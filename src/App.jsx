c6ecf9750108a8ef548ecacb69f9bfda25ddbad5
Zet <111925382+zetmosoma10@users.noreply.github.com>
2024-03-04 16:53:01 +0200

    build Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Button from "./Components/Button";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <main className="container mx-auto px-4 max-w-screen-lg font-display texts-base text-dark_grayish_blue ">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Button />
     </main>
   );
 }
---
+++ src/Components/Button.jsx
@@ -0,0 +1,14 @@
+const Button = () => {
+  return (
+    <div
+      className="bg-soft_red 
+  py-3 px-7 inline-block text-off_white 
+  leading-6 tracking-[4.38px] font-bold uppercase 
+  cursor-pointer hover:scale-105"
+    >
+      read more
+    </div>
+  );
+};
+
+export default Button;
